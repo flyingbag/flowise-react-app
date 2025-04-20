0a05891c9ab2205ca37b7e132d372f55a28ba2f2
Gao Yang <yang.gao@unity3d.com>
2025-04-20 21:57:17 +0800

    Update the entrypoint with a marker

--- flowise-entrypoint.sh
+++ flowise-entrypoint.sh
@@ -1,11 +1,17 @@
 #!/bin/sh
 set -e
 
-# Install desired modules into Flowise’s plugins directory
-npm install $(echo "$TOOL_FUNCTION_EXTERNAL_DEP" | tr ',' ' ') \
-  --prefix /usr/local/lib/node_modules/flowise \
-  --save \
-  --loglevel error
+MARKER="/usr/local/lib/node_modules/flowise/.modules_installed"
+if [ ! -f "$MARKER" ]; then
+  echo "Installing modules into Flowise workspace..."
+  npm install $(echo "$TOOL_FUNCTION_EXTERNAL_DEP" | tr ',' ' ') \
+    --prefix /usr/local/lib/node_modules/flowise \
+    --save \
+    --loglevel error
+  touch "$MARKER"
+else
+  echo "Modules already installed; skipping installation."
+fi
 
 # Exec the official start command
 exec flowise start
